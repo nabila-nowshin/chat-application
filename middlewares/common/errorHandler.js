146af472ccaa478c24cd9586c4e17fe0f42ea662
Nabila Nowshin <nabilanowshin327@gmail.com>
2025-10-30 16:41:58 +0600

    Add errorHandler

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -2,14 +2,21 @@ const createError = require("http-errors");
 
 //404 Not found
 function notFoundHandler(req, res, next) {
-  next(createError("404", "Your requested content Not found"));
+  next(createError(404, "Your requested content Not found"));
 }
 
 //deafult one
 function errorHandler(err, req, res, next) {
-  res.render("error", {
-    title: "error",
-  });
+  res.locals.error = { message: err.message };
+  res.status(err.status || 500);
+
+  if (!res.locals.html) {
+    res.render("error", {
+      title: "error page",
+    });
+  } else {
+    res.json(res.locals.error);
+  }
 }
 
 module.exports = { errorHandler, notFoundHandler };
